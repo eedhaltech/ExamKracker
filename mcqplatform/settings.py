435fa6e64809fedf3d79161c51fa26b2e3ad3b94
eedhaltech <eedhaltech@gmail.com>
2026-06-02 02:30:42 +0530

    Update settings.py

--- mcqplatform/settings.py
+++ mcqplatform/settings.py
@@ -84,14 +84,14 @@ DATABASES = {
 
 # Cache — Redis
 CACHES = {
-    "default": {
-        "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": "redis://127.0.0.1:6379/1",
-        "OPTIONS": {
-            "CLIENT_CLASS": "django_redis.client.DefaultClient",
-        }
+    'default': {
+        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
     }
 }
+
+SESSION_ENGINE = 'django.contrib.sessions.backends.db'
+
+SESSION_ENGINE = 'django.contrib.sessions.backends.cache'
 
 SESSION_ENGINE = 'django.contrib.sessions.backends.cache'
 SESSION_CACHE_ALIAS = 'default'
